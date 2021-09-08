951928e9dfa9c0ff4ba3b8847b270381db6b30e2
guberger <guillaume.berger@uclouvain.be>
2021-09-08 18:16:00 +0200

    Picture compute JSR

--- examples/main_compute_jsr.jl
+++ examples/main_compute_jsr.jl
@@ -11,8 +11,8 @@ sleep(0.1)
 # P = [8.0 0.0; 0.0 1.0]
 # A = P*0.5*(@SMatrix [cos(α) -sin(α); sin(α) cos(α)])/P
 A = @SMatrix [0 1; 1 0]
-A1 = @SMatrix [2 1; 0.001 1]
-A2 = @SMatrix [2 1; -0.001 1]
+A1 = @SMatrix [2 1; 0.1 1]
+A2 = @SMatrix [2 1; -0.1 1]
 
 my_gui = DataDrivenQuadraticJSR.new_gui_compute_jsr((A1, A2))
 
