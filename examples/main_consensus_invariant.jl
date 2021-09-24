8d71e3233dfc58015a488ed831120ced820a35c3
guberger@PC-1r0-013 <guillaume.berger@uclouvain.be>
2021-09-24 18:42:37 +0200

    Example consensus (continued)

--- examples/main_consensus_invariant.jl
+++ examples/main_consensus_invariant.jl
@@ -14,5 +14,20 @@ A1 = SMatrix{8,8}([S1 zeros(4, 4); zeros(4, 4) S1])
 S2 = @SMatrix [α 0 (1-α) 0; 0 α 0 (1-α); (1-α) 0 α 0; 0 (1-α) 0 α]
 A2 = SMatrix{8,8}([S2 zeros(4, 4); zeros(4, 4) S2])
 
+S3 = @SMatrix [α 0 0 (1-α); 0 α (1-α) 0; 0 (1-α) α 0; (1-α) 0 0 α]
+A3 = SMatrix{8,8}([S3 zeros(4, 4); zeros(4, 4) S3])
+
+A = (A1, A2, A3)
+
+nsample = 3000
+x_list = [SVector{8}(randn(8)) for i = 1:nsample]
+σ_list = rand((1, 2, 3), nsample)
+y_list = [A[σ_list[i]]*x_list[i] for i = 1:nsample]
+ηm_list = zeros(nsample)
+ηa_list = zeros(nsample)
+
+γ_opt, P_opt = DataDrivenQuadraticJSR.jsr_quadratic(x_list, y_list,
+    ηm_list, ηa_list, 0.0, 1e2; max_trace=nothing)
+
 print("")
 sleep(0.1)
